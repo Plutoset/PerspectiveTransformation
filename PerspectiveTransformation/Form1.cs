cd1aaf6c474b1ab68ba86157ae15f0172d0a6ef5
Plutoset <1452089861@qq.com>
2021-01-09 18:53:38 +0800

    Save first

--- PerspectiveTransformation/Form1.cs
+++ PerspectiveTransformation/Form1.cs
@@ -44,8 +44,8 @@ namespace PerspectiveTransformation
             UpdateSize();
             //使窗体大小初始全屏化
             KeyPreview = true;
-        //    StartPosition = FormStartPosition.Manual;
-            Rectangle rect = Screen.GetWorkingArea(this);
+            StartPosition = FormStartPosition.Manual;
+            var rect = Screen.GetWorkingArea(this);
             this.Width = rect.Width;
             this.Height = rect.Height;
         }
@@ -108,13 +108,19 @@ namespace PerspectiveTransformation
         }
         private void BStore_Click(object sender, EventArgs e)
         {
-            if (_tranPoints != null && _tranPoints.Count != 0)
+         
+            if(_storedPoints !=null && _storedPoints.Count !=0)
             {
-                _tranPoints.Clear();
+                if (_tranPoints == null) return;
+                if (_tranPoints.Count != 0)
+                {
+                    _tranPoints.Clear();
+                }
                 foreach (var point in _storedPoints)
                 {
                     _tranPoints.Add(point);
                 }
+
             }
             else
             {
@@ -156,9 +162,9 @@ namespace PerspectiveTransformation
         }
         private static string Roll(string inputString)
         {
-            string returnString = "";
-            int n = inputString.Length;
-            for (int i = 0; i < n; i++)
+            var returnString = "";
+            var n = inputString.Length;
+            for (var i = 0; i < n; i++)
             {
                 returnString += inputString[n - i - 1];
             }
@@ -191,16 +197,29 @@ namespace PerspectiveTransformation
             return new Mat(src.Height, src.Width, new MatType(MatType.CV_8UC3), result, step);
 
         }
-        private Image WarpPerspective()
+        private  Image WarpPerspective()
         {
-            Point2f[] warpFromPoints = { _tranPoints[0], _tranPoints[1], _tranPoints[2], _tranPoints[3] };
-            Point2f[] warpToPoints = { _storedPoints[0], _storedPoints[1], _storedPoints[2], _storedPoints[3] };
-            var src = BitmapToMat(_sourceImage);
-            var dst = new Mat();
-            var trans = Cv2.GetPerspectiveTransform(warpFromPoints, warpToPoints);
-            trans.ConvertTo(trans, MatType.CV_32F);
-            Cv2.WarpPerspective(src, dst, trans, new OpenCvSharp.Size() { Height = src.Cols, Width = src.Rows });
-            Image nowImage = dst.ToBitmap();
+            Image nowImage;
+            if (_tranPoints != null && _tranPoints.Count != 0)
+            {
+                var  warpFromPoints = _tranPoints;
+                var warpToPoints =  _storedPoints;
+                for (var index = 0; index < warpToPoints.Count; index++)
+                {
+                    warpToPoints[index] = ToCanvasPoint(warpToPoints[index]);
+                    warpFromPoints[index] = ToCanvasPoint(warpFromPoints[index]);
+                }
+                var src = _sourceImage.ToMat();
+                var dst = new Mat();
+                var trans = Cv2.GetPerspectiveTransform(warpFromPoints, warpToPoints);
+                trans.ConvertTo(trans, MatType.CV_32F);
+                Cv2.WarpPerspective(src, dst, trans, new OpenCvSharp.Size() { Height = src.Cols, Width = src.Rows });
+                nowImage = dst.ToBitmap();
+            }
+            else
+            {
+                nowImage = _sourceImage;
+            }
             return nowImage;
         }
         private void UpdateMap()
@@ -254,6 +273,7 @@ namespace PerspectiveTransformation
 
 
         }
+
         private Point ToScreenPoint(MyPoint onePoint, int scale, int offsetX, int offsetY)
         {
             var X = (int)(onePoint.X * ((scale - 1) * 2 * PanelOrigin.Width / 50 + PanelOrigin.Width)) +
@@ -262,14 +282,14 @@ namespace PerspectiveTransformation
                     offsetY + (1 - scale) * PanelOrigin.Height / 50;
             return new Point(X, Y);
         }
-        public MyPoint ToTruePoint(Point onePoint, int scale, int offsetX, int offsetY)
+        private Point2f ToCanvasPoint(Point2f onePoint)
         {
-            var X = (float)(onePoint.X - offsetX - (1 - scale) * PanelOrigin.Width / 50) /
-                    (float)((scale - 1) * 2 * PanelOrigin.Width / 50 + PanelOrigin.Width);
-            var Y = (float)(onePoint.Y - offsetY - (1 - scale) * PanelOrigin.Height / 50) /
-                    (float)((scale - 1) * 2 * PanelOrigin.Height / 50 + PanelOrigin.Height);
-            return new MyPoint(X, Y);
+            var scale = _sourceImage.Width / PanelOrigin.Width;
+            var X = (float)(onePoint.X * scale);
+            var Y = (float)(onePoint.Y * scale);
+            return new Point2f(X, Y);
         }
+
 
         //绘制变形框
         public void DrawAll(Point startPoint, Point endPoint, Graphics g)
@@ -285,12 +305,15 @@ namespace PerspectiveTransformation
                 
             }
         }
-        private void DrawPoints(List<MyPoint> myPoints, Graphics g)
+
+        private static void DrawPoints(List<MyPoint> myPoints, Graphics g)
         {
+            if (myPoints == null) throw new ArgumentNullException(nameof(myPoints));
+            if (g == null) throw new ArgumentNullException(nameof(g));
             var points = new Point[4];
             foreach (var point in myPoints)
             {
-                Rectangle rect = new Rectangle((int)(point.X - 5), (int)(point.Y - 5), 10, 10);
+                var rect = new Rectangle((int)(point.X - 5), (int)(point.Y - 5), 10, 10);
                 g.DrawEllipse(new Pen(Color.SteelBlue), rect);
                 g.FillEllipse(new SolidBrush(Color.SteelBlue), rect);
                 var i = myPoints.IndexOf(point);
@@ -311,9 +334,10 @@ namespace PerspectiveTransformation
                 new Point(Convert.ToInt32(points[3].X / 3 + points[2].X * 2 / 3), Convert.ToInt32(points[3].Y * 2 / 3 + points[2].Y / 3)));
             
         }
+
         private void DrawBank(Point startPoint, Point endPoint, Graphics g)
         {
-            List<MyPoint> mypoints = new List<MyPoint>();
+            var mypoints = new List<MyPoint>();
             var width = Math.Abs(startPoint.X - endPoint.X);
             var height = Math.Abs(startPoint.Y - endPoint.Y);
             var left = Math.Min(startPoint.X, endPoint.X);
@@ -326,52 +350,57 @@ namespace PerspectiveTransformation
             //绘制边角的四个顶点
             DrawPoints(mypoints,g);
             _myPoints = mypoints;
+            _storedPoints.Clear();
+            foreach (var myPoint in mypoints)
+            {
+                _storedPoints.Add(new Point2f(myPoint.X, myPoint.Y));
+            }
+            
         }
 
         private void TranBank(Point startPoint, Point endPoint, Graphics g)
         {
-            foreach (MyPoint myPoint in _myPoints)
+            if (_storedPoints != null && _storedPoints.Count != 0)
             {
-                _tranPoints.Add(new Point2f(myPoint.X, myPoint.Y));
-            }
-            List<MyPoint> _mypoints = _myPoints;
-            List<Point2f> nowPoints = _tranPoints;
-            
-            //绘制边角的四个顶点
-            Point[] points = new Point[4];
-            for (int i = 0; i < _mypoints.Count; i++)
-            {
-                points[i] = new Point
+                var mypoints = _myPoints;
+                var points = new Point[4];
+                //绘制边角的四个顶点
+                for (var i = 0; i < mypoints.Count; i++)
                 {
-                    X = (int)(_mypoints[i].X),
-                    Y = (int)(_mypoints[i].Y)
-                };
-                var distance = Math.Pow((_mypoints[i].X - startPoint.X), 2) 
-                               + Math.Pow((_mypoints[i].Y - startPoint.Y), 2);
-                if (distance <= 25)
-                {
-                    points[i] = endPoint;
-                    _changeNumber = i;
-                    nowPoints[i] = new Point2f(
-                        endPoint.X, 
-                        endPoint.Y
-                        );
-                    _mypoints[i] = new MyPoint(
-                        startPoint.X,
-                        startPoint.Y
-                        );
+                    points[i] = new Point
+                    {
+                        X = (int)(mypoints[i].X),
+                        Y = (int)(mypoints[i].Y)
+                    };
+                    var distance = Math.Pow((mypoints[i].X - startPoint.X), 2) + Math.Pow((mypoints[i].Y - startPoint.Y), 2);
+                    if (distance <= 25)
+                    {
+                        points[i] = endPoint;
+                        _changeNumber = i;
+                    }
+                    var rect = new Rectangle(points[i].X - 5, points[i].Y - 5, 10, 10);
+                    g.DrawEllipse(new Pen(Color.SteelBlue), rect);
+                    g.FillEllipse(new SolidBrush(Color.SteelBlue), rect);
                 }
-                
+                g.DrawPolygon(new Pen(Color.SteelBlue, 2), points);
+                g.DrawLine(new Pen(Color.SteelBlue), new Point(Convert.ToInt32(points[0].X * 2 / 3 + points[3].X / 3), Convert.ToInt32(points[0].Y * 2 / 3 + points[3].Y / 3)),
+                       new Point(Convert.ToInt32(points[1].X * 2 / 3 + points[2].X / 3), Convert.ToInt32(points[1].Y * 2 / 3 + points[2].Y / 3)));
+                g.DrawLine(new Pen(Color.SteelBlue), new Point(Convert.ToInt32(points[0].X / 3 + points[3].X * 2 / 3), Convert.ToInt32(points[0].Y / 3 + points[3].Y * 2 / 3)),
+                       new Point(Convert.ToInt32(points[1].X / 3 + points[2].X * 2 / 3), Convert.ToInt32(points[1].Y / 3 + points[2].Y * 2 / 3)));
+                g.DrawLine(new Pen(Color.SteelBlue), new Point(Convert.ToInt32(points[0].X * 2 / 3 + points[1].X / 3), Convert.ToInt32(points[0].Y * 2 / 3 + points[1].Y / 3)),
+                       new Point(Convert.ToInt32(points[3].X * 2 / 3 + points[2].X / 3), Convert.ToInt32(points[3].Y * 2 / 3 + points[2].Y / 3)));
+                g.DrawLine(new Pen(Color.SteelBlue), new Point(Convert.ToInt32(points[0].X / 3 + points[1].X * 2 / 3), Convert.ToInt32(points[0].Y / 3 + points[1].Y * 2 / 3)),
+                       new Point(Convert.ToInt32(points[3].X / 3 + points[2].X * 2 / 3), Convert.ToInt32(points[3].Y / 3 + points[2].Y * 2 / 3)));
+
+
             }
-            DrawPoints(_mypoints,g);
-            _myPoints = _mypoints;
-            Point2f[] warpFromPoints = { _tranPoints[0], _tranPoints[1], _tranPoints[2], _tranPoints[3] };
-            Point2f[] warpToPoints = { nowPoints[0], nowPoints[1], nowPoints[2], nowPoints[3] };
-       //     src = OpenCvSharp.Extensions.BitmapConverter.ToMat(_sourceImage);
-         //   dst = src;
-      //      Mat Trans = Cv2.GetAffineTransform(warpFromPoints, warpToPoints);
-      //      Cv2.WarpPerspective(src,dst,Trans,src.Size());
+            else
+            {
+                MessageBox.Show("请创建变形框！");
+            }
+               
         }
+
 
         private void PanelOrigin_Paint(object sender, PaintEventArgs e)
         {
@@ -385,17 +414,19 @@ namespace PerspectiveTransformation
             }
             Invalidate();
         }
+
         private void PanelShown_Paint(object sender, PaintEventArgs e)
         {
             Invalidate();
         }
+
+
         private void PanelOrigin_MouseDown(object sender, MouseEventArgs e)
         {
             if (e.Button != MouseButtons.Left) return;
             if (_fromPoint != null)
             {
                 _fromPoint = null;
-               // UpdateMap();
             }
             _mouseFrom = e.Location;
             _fromPoint = new MyPoint(e.X, e.Y);
@@ -409,17 +440,22 @@ namespace PerspectiveTransformation
             Drawcord = false;
             if (tranRadioButton.Checked == true && tranRadioButton.Enabled == true)
             {
-                if(_changeNumber != null) _myPoints[_changeNumber.Value] = _toPoint;
+                if (_changeNumber != null)
+                {
+                    _storedPoints[_changeNumber.Value] = new Point2f(e.X,e.Y);
+                    _myPoints[_changeNumber.Value] = new MyPoint(e.X, e.Y);
+                }
             }
             _changeNumber = null;
-         //   if(dst!=null)Cv2.ImShow("dst",dst);
         }
+
         private void PanelShown_MouseMove(object sender, MouseEventArgs e)
         {
             LocationLabel.Text = e.Location.ToString();
             if (e.Button != MouseButtons.Left) return;
             PanelShown.Invalidate();
         }
+
 
         private void Form1_Size(object sender, EventArgs e)
         {
